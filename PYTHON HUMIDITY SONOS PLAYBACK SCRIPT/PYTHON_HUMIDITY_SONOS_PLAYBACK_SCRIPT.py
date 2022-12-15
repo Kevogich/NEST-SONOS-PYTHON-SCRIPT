6e1c001eb238ea0f2a1c81b307514fb018693102
Kevin Gichia <Kevogich@live.com>
2022-12-15 15:54:09 +0100

    Rebase

--- PYTHON HUMIDITY SONOS PLAYBACK SCRIPT/PYTHON_HUMIDITY_SONOS_PLAYBACK_SCRIPT.py
+++ PYTHON HUMIDITY SONOS PLAYBACK SCRIPT/PYTHON_HUMIDITY_SONOS_PLAYBACK_SCRIPT.py
@@ -1,1 +1,78 @@
+# Import necessary modules
+import os
+import requests
+
+# Set the Nest API endpoint
+NEST_ENDPOINT = 'https://developer-api.nest.com/devices/thermostats'
+
+# Set the Sonos API endpoint
+SONOS_ENDPOINT = 'http://localhost:5005/'
+
+# Set the path to the custom voice recording
+VOICE_RECORDING_PATH = '/path/to/voice/recording.mp3'
+
+# Set the access token for the Nest API
+NEST_ACCESS_TOKEN = 'YOUR_NEST_ACCESS_TOKEN'
+
+# Set the access token for the Sonos API
+SONOS_ACCESS_TOKEN = 'YOUR_SONOS_ACCESS_TOKEN'
+
+# Set the device ID of the Sonos speaker
+SONOS_DEVICE_ID = 'YOUR_SONOS_DEVICE_ID'
+
+# Set the threshold for the humidity level
+HUMIDITY_THRESHOLD = 50
+
+# Set the headers for the Nest API request
+nest_headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + NEST_ACCESS_TOKEN
+}
+
+# Set the headers for the Sonos API request
+sonos_headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + SONOS_ACCESS_TOKEN
+}
+
+# Function to play the voice recording on the Sonos speaker
+def play_voice_recording(device_id, recording_path):
+    # Set the endpoint for the playback request
+    endpoint = SONOS_ENDPOINT + 'playback/' + device_id
+
+    # Set the payload for the playback request
+    payload = {
+        'media_uri': 'x-file-cifs://' + recording_path
+    }
+
+    # Send the playback request to the Sonos API
+    response = requests.post(endpoint, json=payload, headers=sonos_headers)
+
+    # Print the response from the Sonos API
+    print(response.text)
+
+# Function to check the humidity level and play the voice recording if necessary
+def check_humidity_and_play():
+    # Send a request to the Nest API to get the current humidity level
+    response = requests.get(NEST_ENDPOINT, headers=nest_headers)
+
+    # Parse the response from the Nest API
+    data = response.json()
+
+    # Get the humidity level
+    humidity = data['thermostat']['humidity']
+
+    # Check if the humidity level is above the threshold
+    if humidity > HUMIDITY_THRESHOLD:
+        # Play the voice recording on the Sonos speaker
+        play_voice_recording(SONOS_DEVICE_ID, VOICE_RECORDING_PATH)
+
+# Main function
+def main():
+    # Check the humidity level and play the voice recording if necessary
+    check_humidity_and_play()
+
+# Run the main function
+if __name__ == '__main__':
+    main()
 
